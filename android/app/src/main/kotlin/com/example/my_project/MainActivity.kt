1c0d74895cc254337b3fd4579217c563e35679e3
samuelkchris <samuelkchris@gmail.com>
2022-09-29 07:31:31 +0300

    initial commit before production

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,7 @@
+package flutter.flow
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
